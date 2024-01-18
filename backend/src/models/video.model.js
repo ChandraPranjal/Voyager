ef0fd6b7abba38a3774905b7444887a2ccda8579
ChandraPranjal <iampranjalchandra@gmail.com>
2024-01-19 00:53:44 +0530

    models upd

---
+++ backend/src/models/video.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const videoSchema = mongoose.Schema({
+  videoFile: {
+    type: String,
+    required: true
+  },
+  thumbnail: {
+    type: String,
+    required: true
+  },
+  owner:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  views: {
+    type: Number,
+    default:0,
+    required: true,
+  },
+  isPublished: {
+    type: Boolean,
+    default:true,
+    required: true,
+  }
+},{timestamps:true});
+
+export const Video = mongoose.model("Video",videoSchema)
